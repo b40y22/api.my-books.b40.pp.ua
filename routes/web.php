da34d6ead744413ba463de77b40170b7e512ee9e
Borshch <b0980652405@gmail.com>
2023-09-26 18:04:48 +0300

    simple fix cs

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-//Route::get('/pdf/book/first-page', function () {
-//    return view('pdf.book.first-page', [
-//        'title' => 'Нимфа лунного моря',
-//        'authors' => ['Вера Петрук'],
-//        'cover' => 'http://loveread.ec/img/photo_books/109941.jpg',
-//        'year' => 2023
-//    ]);
-//});
